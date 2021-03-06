953386b769c1350f42190fcb7c79ccd39dc0e17c
Chad Charlesworth <charlesworthchad@gmail.com>
2021-03-05 23:31:43 -0500

    Added Sql inserts for starter data

---
+++ Server/database/StarterData.sql
@@ -0,0 +1,9 @@
+INSERT INTO MotionPictures (Name, Description, Release_Year)
+VALUES ('The Shawshank Redemption', 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.
+', 1994),
+('The Godfather', 'An organized crime dynasty''s aging patriarch transfers control of his clandestine empire to his reluctant son.', 1972),
+('The Dark Knight', 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.', 2008),
+('12 Angry Men', 'A jury holdout attempts to prevent a miscarriage of justice by forcing his colleagues to reconsider the evidence.', 1957),
+('Schindler''s List', 'In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.
+', 1993)
+
